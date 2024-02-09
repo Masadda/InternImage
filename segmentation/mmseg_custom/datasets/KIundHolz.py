76fb47b2776082ad77ff8cb060cb3ea8af96af8a
kammerbauerro76348 <kammerbauerro76348@th-nuernberg.de>
2024-02-09 12:58:54 +0100

    add pipeline-approach configs

--- segmentation/mmseg_custom/datasets/KIundHolz.py
+++ segmentation/mmseg_custom/datasets/KIundHolz.py
@@ -43,3 +43,21 @@ class KIundHolzDataset_no_fv(CustomDataset):
             seg_map_suffix=seg_map_suffix,
             reduce_zero_label=reduce_zero_label,
             **kwargs)
+
+class KIundHolzDataset_pipeline(CustomDataset):
+    """KIundHolz dataset.
+    """
+    CLASSES = ("Background", "Fäule", "Fäule(vielleicht)", "Druckholz", "Verfärbung", "Einwuchs_Riss")
+
+    PALETTE = [[0,0,0], [255, 0, 0], [255, 128, 0], [255, 255, 0], [0, 0, 255], [32, 32, 32]]
+
+    def __init__(self,
+                 img_suffix='.jpg',
+                 seg_map_suffix='.png',
+                 reduce_zero_label=False,
+                 **kwargs) -> None:
+        super().__init__(
+            img_suffix=img_suffix,
+            seg_map_suffix=seg_map_suffix,
+            reduce_zero_label=reduce_zero_label,
+            **kwargs)
--- segmentation/mmseg_custom/datasets/__init__.py
+++ segmentation/mmseg_custom/datasets/__init__.py
@@ -3,9 +3,9 @@ from .mapillary import MapillaryDataset  # noqa: F401,F403
 from .nyu_depth_v2 import NYUDepthV2Dataset  # noqa: F401,F403
 from .pipelines import *  # noqa: F401,F403
 from .dataset_wrappers import ConcatDataset
-from .KIundHolz import KIundHolzDataset_full, KIundHolzDataset_no_fv
+from .KIundHolz import KIundHolzDataset_full, KIundHolzDataset_no_fv, KIundHolzDataset_pipeline
 
 
 __all__ = [
-    'MapillaryDataset', 'NYUDepthV2Dataset', 'ConcatDataset', 'KIundHolzDataset_full', 'KIundHolzDataset_no_fv'
+    'MapillaryDataset', 'NYUDepthV2Dataset', 'ConcatDataset', 'KIundHolzDataset_full', 'KIundHolzDataset_no_fv', 'KIundHolzDataset_pipeline'
 ]
